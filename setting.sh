2a9a08232c9405948f5e9e77e296686a66cb1797
YUJI MORI <vagrant@ubuntu-13.10-server-amd64>
2013-11-10 12:01:16 +0900

    setting.shの追加

---
+++ setting.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ln -s ~/myconfigfile/.vimrc ~/.vimrc
+ln -s ~/myconfigfile/.jshintrc ~/.jshintrc
+ln -s ~/myconfigfilea/.zshrc ~/.zshrc
+ln -s ~/myconfigfile/.tmux.conf ~/.tmux.conf
+ln -s ~/myconfigfile/.gitconfig ~/.gitconfig
+ln -s ~/myconfigfile/.ctags ~/.ctags
